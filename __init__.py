6d40660b2c52b24afdfa95383aa236bbd10219a5
Carlos Aguilar <77519735+ssbucarlos@users.noreply.github.com>
2023-11-17 13:20:34 -0600

    Implement "version" field

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ bl_info = {
     'category': 'All',
     'location': 'View 3D > Tool Shelf > Ultimate',
     'description': 'A collection of tools for importing models and animations to smash ultimate.',
-    'version': (0, 9, 0),
+    'version': (2023, 11, 17, 0),
     'blender': (3, 3, 0),
     'warning': 'TO REMOVE: First "Disable" the plugin, then restart blender, then you can hit "Remove" to uninstall',
     'doc_url': 'https://github.com/ssbucarlos/smash-ultimate-blender/wiki',
