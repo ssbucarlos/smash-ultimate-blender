e7827aec24b564661f08413b517e23dc09b15b6c
Carlos Aguilar <77519735+ssbucarlos@users.noreply.github.com>
2024-06-17 12:47:53 -0500

    improved updater to only check for compatible versions

--- source/updater/ui.py
+++ source/updater/ui.py
@@ -1,5 +1,4 @@
 from bpy.types import Panel
-from . import version_check
 
 class SUB_PT_update_plugin(Panel):
     bl_space_type = 'VIEW_3D'
@@ -9,20 +8,18 @@ class SUB_PT_update_plugin(Panel):
 
     @classmethod
     def poll(cls, context):
-        from ...__init__ import bl_info
-        current_version = bl_info['version']
-        if version_check.LATEST_VERSION:
-            return current_version < version_check.LATEST_VERSION
-        else:
-            return False
+        from .version_check import COMPATIBLE_UPDATE_AVAILABLE
+        return COMPATIBLE_UPDATE_AVAILABLE
     
     def draw(self, context):
         from ...__init__ import bl_info
+        from .version_check import LATEST_COMPATIBLE_VERSION
+
         layout = self.layout
         layout.use_property_split = False
         
-        layout.row().label(text="An update is available!")
+        layout.row().label(text="An update compatible with your current blender version is available!")
         layout.row().label(text="Please download it from github when you get the chance :) ")
         current_version = bl_info['version']
         layout.row().label(text=f"Current version = {current_version}")
-        layout.row().label(text=f"Latest version  = {version_check.LATEST_VERSION}")
+        layout.row().label(text=f"Latest compatible version  = {LATEST_COMPATIBLE_VERSION}")
